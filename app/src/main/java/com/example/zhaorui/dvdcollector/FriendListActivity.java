26087653aa0578b459e604904b1e0eae2d07e0b0
Jiaxuan Yue <jiaxuan2@cs.ualberta.ca>
2015-10-08 21:30:36 -0600

    UI finished

--- app/src/main/java/com/example/zhaorui/dvdcollector/FriendListActivity.java
+++ app/src/main/java/com/example/zhaorui/dvdcollector/FriendListActivity.java
@@ -1,16 +1,37 @@
 package com.example.zhaorui.dvdcollector;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class FriendListActivity extends BaseActivity {
+    private String[] data = { "Jack", "Lucy", "Calvin", "Frank", "Mike"};
+    Button btnMenuMyInvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(FriendListActivity.this, android.R.layout.simple_list_item_1, data);
+        ListView listView = (ListView) findViewById(R.id.listViewFriendList);
+        listView.setAdapter(adapter);
+
+        // open the menu
+        btnMenuMyInvent = (Button)findViewById(R.id.btn_title_my_friends);
+        btnMenuMyInvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOptionsMenu();
+            }
+        });
     }
 
     @Override
@@ -18,6 +39,8 @@ public class FriendListActivity extends BaseActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_friend_list, menu);
         return true;
+
+
     }
 
     @Override
@@ -28,10 +51,21 @@ public class FriendListActivity extends BaseActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.friendlist_add){
+            final EditText editText = new EditText(this);
+            final AlertDialog.Builder alert=new AlertDialog.Builder(this);
+            alert.setTitle("Please input a username  ")
+                    .setView(editText)
+                    .setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            alert.show();
         }
 
         return super.onOptionsItemSelected(item);
     }
 }
+
